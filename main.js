0ca0812935bcaa517024b234b684a047eabad7e1
Ralgo894 <cerberus894@icloud.com>
2022-05-13 07:06:46 +0900

    a

--- main.js
+++ main.js
@@ -1,17 +1,26 @@
 function appendScript(URL) {
-	var el = document.createElement('script');
-	el.src = URL;
-	document.body.appendChild(el);
+	return new Promise(function(resolve, reject) {
+		var el = document.createElement('script');
+		el.src = URL;
+		document.body.appendChild(el);
+
+		el.addEventListener("load", resolve);
+	});
 };
 
 function appendCss(URL) {
-	var el = document.createElement('link');
-	el.href = URL;
-	el.rel = 'stylesheet';
-	el.type = 'text/css';
-	// HEAD要素の最後に追加
-	document.getElementsByTagName('head')[0].appendChild(el);
+	return new Promise(function(resolve, reject) {
+		var el = document.createElement('link');
+		el.href = URL;
+		el.rel = 'stylesheet';
+		el.type = 'text/css';
+		// HEAD要素の最後に追加
+		document.getElementsByTagName('head')[0].appendChild(el);
+		el.addEventListener("load", resolve);
+	});
 }
 
-appendScript("https://ralgo894.github.io/p/dbStorage.js");
-appendScript("https://ralgo894.github.io/p/pGetData/bookmark.js");
+appendScript("https://ralgo894.github.io/p/dbStorage.js")
+.then(() => {
+	appendScript("https://ralgo894.github.io/p/pGetData/bookmark.js");
+});
